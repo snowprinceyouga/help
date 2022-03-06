59a4472d4288675c1575ae62f43857373c219b80
Lapenok <osharapova@yandex.com>
2022-03-07 00:12:32 +0300

    start project

---
+++ src/main/java/helpdesk/ProjectHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectHelpdeskApplication.class, args);
+	}
+
+}
