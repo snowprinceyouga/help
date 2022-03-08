2b4435a825effa7f1e71bd5506e13b00f73d2e05
ArtemMikishev <artemmikishev94@gmail.com>
2022-03-08 17:49:52 +0300

    added pojo class

--- src/main/java/helpdesk/pojo/Ticket.java
+++ src/main/java/helpdesk/pojo/Ticket.java
@@ -10,6 +10,73 @@ import javax.persistence.Table;
 public class Ticket {
 
     @Id
-    @Column(name = "id")
-    private Integer id;
+    @Column(name = "ticket_id")
+    private Integer ticketId;
+
+
+    @Column(name = "client_id")
+    private Integer clientId;
+
+    @Column(name = "support_id")
+    private Integer supportId;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+
+    public Ticket(){
+    }
+
+    public Integer getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Integer clientId) {
+        this.clientId = clientId;
+    }
+
+    public Integer getSupportId() {
+        return supportId;
+    }
+
+    public void setSupportId(Integer supportId) {
+        this.supportId = supportId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketId=" + ticketId +
+                ", clientId=" + clientId +
+                ", supportId=" + supportId +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
