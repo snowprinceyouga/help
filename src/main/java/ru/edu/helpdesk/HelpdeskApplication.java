41f3b2317016fdcc4e17843772f902343d40c0c9
Malyshev Sergey <in1t@ya.ru>
2022-03-16 02:24:16 +0300

    Админка, список пользователей

--- src/main/java/ru/edu/helpdesk/HelpdeskApplication.java
+++ src/main/java/ru/edu/helpdesk/HelpdeskApplication.java
@@ -2,8 +2,10 @@ package ru.edu.helpdesk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class HelpdeskApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/edu/helpdesk/controller/AdminController.java
+++ src/main/java/ru/edu/helpdesk/controller/AdminController.java
@@ -1,22 +1,34 @@
 package ru.edu.helpdesk.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.edu.helpdesk.entity.User;
 import ru.edu.helpdesk.service.AdminService;
 
+import java.security.Principal;
 import java.util.List;
 
-@RestController
+@Controller
+@Secured("ROLE_ADMIN")
 @RequestMapping(value = "/admin")
 public class AdminController {
     @Autowired
     private AdminService adminService;
 
-    @GetMapping
-    public List<User> index() {
-        return adminService.usersList();
+    @GetMapping()
+    public String index(Model model, Principal principal) {
+        if (principal != null) {
+            final String login = principal.getName();
+            final List<User> users = adminService.usersList();
+            model.addAttribute("username", login);
+            model.addAttribute("users", users);
+        } else {
+            model.addAttribute("username", "Anonymous");
+        }
+        return "admin/index";
     }
 }
